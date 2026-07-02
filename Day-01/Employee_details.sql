798d047779dfa03591a6da47dba669e7b8cd3b14
Priyanshu Mishra <manmohan94540@gmail.com>
2026-07-02 18:37:24 +0530

    Day 1: Added SQL practice

---
+++ Day-01/Employee_details.sql
@@ -0,0 +1,15 @@
+create database EMP;
+use EMP;
+create table EDT (
+Sid int,
+Sname char(10),Sfee Decimal(6,2),
+age Tinyint);
+
+Sp_HELP EDT;
+Sp_HELP Dept_Details;
+Sp_HELP EMP_DETAILS;
+
+select * from EMP_DETAILS;
+
+
+
